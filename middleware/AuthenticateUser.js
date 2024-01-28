3ef56c3889a3186d94fc327d5b53f54e5aa85587
LeroyBG <leroylightning@g.ucla.edu>
2024-01-28 13:45:42 -0800

    add authentication middleware

---
+++ middleware/AuthenticateUser.js
@@ -0,0 +1,25 @@
+import admin from 'firebase-admin'
+
+// verify firebase JWT token
+export async function authenticate(req, res, next) {
+    const headerToken = req.headers.authorization
+    if (!headerToken) {
+        res.status(401).send('no token provided')
+        return
+    }
+    try {
+        if (headerToken?.split(' ')[0] !== "Bearer") {
+            res.status(401).send("invalid token")
+            return
+        }
+        const token = headerToken.split(' ')[1]
+        const decoded = await admin.auth().verifyIdToken(token)
+        if (decoded) {
+            return next()
+        }
+        res.status(401).send('unauthorized')
+    } catch (err) {
+        console.log(err)
+        res.status(500).send(err)
+    }
+}
--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 import express from "express"
 import cors from 'cors'
 import dotenv from 'dotenv'
+import { applicationDefault, initializeApp } from "firebase-admin/app"
 
 // configure environment variables
 dotenv.config()
 const PORT = process.env.PORT ?? 3005
+const DATABASE_URL = process.env.DATABASE_URL
 
-// initialize app
+// initialize express app
 const app = express()
 app.use(cors())
+
+// initialize firebase app
+initializeApp({
+    credential: applicationDefault(),
+    databaseURL: DATABASE_URL
+})
 
 app.use('/', (req, res) => {
     res.send('Hi!')
