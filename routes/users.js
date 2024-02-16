24f06810e17110be4be8d181169b80e1ef0446b0
LeroyBG <leroylightning@g.ucla.edu>
2024-02-16 01:29:02 -0800

    add user creation route

---
+++ routes/users.js
@@ -0,0 +1,66 @@
+import { getAuth } from 'firebase-admin/auth'
+import express, { Router }  from 'express'
+import Joi from 'joi'
+import { db } from '../config/firebase-config.js'
+
+import { cookbookSchema } from '../schema/cookbook.js'
+import { userSchema } from '../schema/user.js'
+
+const router = express.Router()
+
+router.post('/', async (req, res, next) => {
+    try {
+        const candidate = {
+            name: req.body.userName,
+            password: req.body.password,
+        }
+    
+        // validate req
+        // right now, username and password are just unconstrained strings
+        await Joi.string().validateAsync(candidate.name)
+        await Joi.string().validateAsync(candidate.password)
+
+        await userSchema.validateAsync(candidate)
+
+        const auth = getAuth()
+
+        const newUser = await auth.createUser({
+            displayName: candidate.name,
+            password: candidate.password
+        })
+
+        const newCookbooks = {
+            originals: {
+                owner: newUser.uid,
+                name: 'Originals',
+                recipes: []
+            },
+            favorites: {
+                owner: newUser.uid,
+                name: 'Favorites',
+                recipes: []
+            }
+        }
+
+        cookbookSchema.validateAsync(newCookbooks.originals)
+        cookbookSchema.validateAsync(newCookbooks.favorites)
+
+        await db.collection('cookbooks').add(newCookbooks.originals)
+        await db.collection('cookbooks').add(newCookbooks.favorites)
+
+        res.sendStatus(201)
+        return
+    } catch (err) {
+        if (err.isJoi && err.name === 'ValidationError') {
+            console.log(err)
+            res.status(400).send('invalid request')
+            return
+        }
+
+        res.status(500).send('err')
+        console.log(err)
+        return
+    }
+})
+
+export default router
